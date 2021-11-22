5d59556996c90f79b3f44e1ddaf2bb9faa48ec51
Zach Griffin <zgriff13@gmail.com>
2021-11-22 14:32:13 -0800

    menu and game manager singleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+
+    //private void Awake()
+    //{
+    //    if (instance != null)
+    //    {
+    //        Destroy(gameObject);
+    //    }
+
+    //    else
+    //    {
+    //        instance = this;
+    //        DontDestroyOnLoad(gameObject);
+    //    }
+
+    //}
+
+    public void SceneLoad(string scene) 
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null)
+                {
+                    GameObject newGO = new GameObject();
+                    _instance = newGO.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        _instance = this as T;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+}
